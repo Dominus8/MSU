cf6503a0a5b61ae7fc3b0dddf5861db271c1f43d
Dominus8 <ikari162@gmail.com>
2022-08-03 00:28:45 +0300

    +app-product single page

--- MSU/resources/static/js/main.js
+++ MSU/resources/static/js/main.js
@@ -1,4 +1,35 @@
-const swiper = new Swiper('.swiper', {
+let swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+
+        },
+
+    }
+});
+
+
+let swiper_single_page = new Swiper('.swiper-single-page', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
