7a81cd0bc42e0b298d9a9fc58cb1e4973ce1f039
Dominus8 <ikari162@gmail.com>
2022-08-04 13:00:16 +0300

    +app-hard-product-singlepage

--- MSU/resources/static/js/main.js
+++ MSU/resources/static/js/main.js
@@ -58,3 +58,33 @@ let swiper_single_page = new Swiper('.swiper-single-page', {
 
     }
 });
+
+let swiper_single_page__bottom = new Swiper('.swiper-single-page__bottom', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+
+        },
+
+    }
+});
